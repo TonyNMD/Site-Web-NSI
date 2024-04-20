4f58c0a84c2b5d2efe592fffb9da1876ff8d78c7
TonyNMD <127332681+TonyNMD@users.noreply.github.com>
2024-04-20 14:34:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content')
+const tabs = document.querySelectorAll("[data-tab-target]")
+const tabContents = document.querySelectorAll("[data-tab-content]")
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
+tabs.forEach(tab => { // boucles pour changer la page affichée
+    tab.addEventListener("click", () => {
         const target = document.querySelector(tab.dataset.tabTarget)
+
         tabContents.forEach(tabContent => {
-            tabContent.classList.remove('active')
+            tabContent.classList.remove("active") // enleve "active"
         })
+
         tabs.forEach(tab => {
-            tab.classList.remove('active')
+            tab.classList.remove("active") // enleve "active"
         })
-        tab.classList.add('active')
-        target.classList.add('active')
+
+        tab.classList.add("active") // ajoute "active" pour changer de page
+        target.classList.add("active") // ajoute "active" pour changer de page
     })
 })
 
-function affiche_commentaire(formulaire) {
+function affiche_commentaire(formulaire) { // fonction pour afficher le commentaire
     var commentaire = document.formulaire.input_commentaire.value;
+
     document.getElementById("txt_commentaire").innerHTML = " → " + commentaire
 }
